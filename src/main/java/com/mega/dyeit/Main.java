158a939385426c3eac0725e0b1b9c8a89627adea
Mega32K <1830877341@qq.com>
2022-10-23 14:12:22 +0800

    first commit

---
+++ src/main/java/com/mega/dyeit/Main.java
@@ -0,0 +1,10 @@
+package com.mega.dyeit;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("dye_it")
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Main {
+    public static final String MOD_ID = "dye_it";
+}
